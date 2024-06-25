31cf3f7ef4a12c77f650ca8579ab9769ff9b2f38
Nahid-web <nahidamin266@gmail.com>
2024-06-25 09:58:02 +0600

    lesson7 done

--- index.js
+++ index.js
@@ -7,26 +7,24 @@
  */
 
 // dependencies
-const http = require('http');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
-const { handleReqRes } = require('./helpers/handleRegRes');
-const environmentToExport = require('./helpers/environments');
-const data = require('./lib/data');
-const { sendTwilioSms } = require('./helpers/notifications');
 // App object -module scattering
 const app = {};
 
 // configuration
 
 // create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environmentToExport.port, () => {
-        console.log(`Listening to port ${environmentToExport.port}`);
-    });
+app.init = () => {
+    // start the sever
+    server.init();
+
+    // start the worker
+    workers.init();
 };
 
-// handle Request Response
-app.handleReqRes = handleReqRes;
-// start the server
-app.createServer();
+app.init();
+
+// export the app
+module.exports = app;
